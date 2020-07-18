a9755a11237cb2d60892f27c63cb45558c7a95c2
zoumaotai <zoumaotai@ailongma.com>
2020-07-19 01:16:03 +0800

    修复了backend的格式

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
     <router-view/>
 
     <!-- FORK Me -->
-    <a href="https://github.com/gtalarico/flask-vuejs-template"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
+    <a href="https://github.com/zoumt1633/flask-vuejs-template"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
 
   </div>
 </template>
